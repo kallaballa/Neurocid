61ffd471ae7f2c0790077e28a37995602700343f
kallaballa <amir@viel-zu.org>
2014-03-08 17:31:19 +0100

    fix forward declations

--- src/util.cpp
+++ src/util.cpp
@@ -6,9 +6,34 @@
  */
 
 #include "util.hpp"
+#include "population.hpp"
+#include "genetic.hpp"
 #include <cstdlib>
 
 namespace tankwar {
+
+Population makePopulation(size_t teamID, size_t size, BrainLayout layout) {
+	Population p;
+	for(size_t i = 0; i < size; i++) {
+		Tank t(teamID, layout, {0, 0}, 0);
+		t.brain_.randomize();
+		p.push_back(t);
+	}
+	return p;
+}
+
+vector<Population> makeTeams(size_t numTeams, size_t teamSize, BrainLayout l) {
+	vector<Population> teams(numTeams);
+	size_t teamID = 0;
+	std::generate(teams.begin(), teams.end(), [&]() { return makePopulation(teamID++, teamSize, l); });
+	return teams;
+}
+
+vector<GeneticPool> makePools(size_t numTeams, GeneticParams gp) {
+	vector<GeneticPool> pools(numTeams);
+	std::generate(pools.begin(), pools.end(), [&]() { return GeneticPool(gp); });
+	return pools;
+}
 
 int iRand(int x,int y) {
 	return rand()%(y-x+1)+x;
--- src/util.hpp
+++ src/util.hpp
@@ -1,37 +1,20 @@
-/*
- * util.hpp
- *
- *  Created on: Mar 2, 2014
- *      Author: elchaschab
- */
-
 #ifndef UTIL_HPP_
 #define UTIL_HPP_
 
+#include <vector>
+#include <stddef.h>
+
 namespace tankwar {
 
-Population makePopulation(size_t teamID, size_t size, BrainLayout layout) {
-	Population p;
-	for(size_t i = 0; i < size; i++) {
-		Tank t(teamID, layout, {0, 0}, 0);
-		t.brain_.randomize();
-		p.push_back(t);
-	}
-	return p;
-}
+using std::vector;
+class Population;
+class GeneticPool;
+class GeneticParams;
+class BrainLayout;
 
-vector<Population> makeTeams(size_t numTeams, size_t teamSize, BrainLayout l) {
-	vector<Population> teams(numTeams);
-	size_t teamID = 0;
-	std::generate(teams.begin(), teams.end(), [&]() { return makePopulation(teamID++, teamSize, l); });
-	return teams;
-}
-
-vector<GeneticPool> makePools(size_t numTeams, GeneticParams gp) {
-	vector<GeneticPool> pools(numTeams);
-	std::generate(pools.begin(), pools.end(), [&]() { return GeneticPool(gp); });
-	return pools;
-}
+Population makePopulation(size_t teamID, size_t size, BrainLayout layout);
+vector<Population> makeTeams(size_t numTeams, size_t teamSize, BrainLayout l);
+vector<GeneticPool> makePools(size_t numTeams, GeneticParams gp);
 
 double fRand(double fMin, double fMax);
 int iRand(int x,int y);
