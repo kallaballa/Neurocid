f0f11406567f8f01159673e173538de86367bf39
kallaballa <amir@viel-zu.org>
2014-04-06 16:54:23 +0200

    exit on first error in build_deps.sh

--- build_deps.sh
+++ build_deps.sh
@@ -1,5 +1,10 @@
+#/bin/bash
+
+set -e 
+
 cd guichan; cmake .; make
 cd ../box2d; cmake -DBOX2D_BUILD_SHARED=ON -DBOX2D_BUILD_STATIC=OFF -DBOX2D_BUILD_EXAMPLES=OFF -DBOX2D_INSTALL=OFF .; make 
 cd ../fann; cmake .; make
 make -C ../kmlocal-1.7.2 
+
 
