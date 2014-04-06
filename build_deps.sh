6fc4cd9d2d4517bed0c01e224ee8dfe0abbf6a5d
kallaballa <amir@viel-zu.org>
2014-04-06 16:03:25 +0200

    updated build_deps

--- build_deps.sh
+++ build_deps.sh
@@ -1,3 +1,4 @@
+cd guichan; cmake .; make
 cd box2d; cmake -DBOX2D_BUILD_SHARED=ON -DBOX2D_BUILD_STATIC=OFF -DBOX2D_BUILD_EXAMPLES=OFF -DBOX2D_INSTALL=OFF .; make 
 cd ../fann; cmake .; make
 make -C ../kmlocal-1.7.2 
