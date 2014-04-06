9f09a5e9e356421af17f551daf22f293426a7284
kallaballa <amir@viel-zu.org>
2014-04-06 16:00:34 +0200

    fixed script paths

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:src/:kmlocal-1.7.2/src/:fann/src:box2d/Box2D
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:src/:kmlocal-1.7.2/src/:fann/src:box2d/Box2D:guichan/
 
 $@
 
