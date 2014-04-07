c96b65a0a24a8e4f4c8b2f46d35634385b14c311
kallaballa <amir@viel-zu.org>
2014-04-07 13:44:01 +0200

    updated build_deps scripts

--- build_deps.sh
+++ build_deps.sh
@@ -1,8 +1,11 @@
 #/bin/bash
 
-set -e 
+cd guichan; make clean; rm -r CMakeFiles; rm CMakeCache.txt
+cd ../box2d/; make clean; rm -r CMakeFiles; rm CMakeCache.txt
+cd ../fann/; make clean;  rm -r CMakeFiles; rm CMakeCache.txt
+make -C ../kmlocal-1.7.2/ clean
 
-cd guichan; cmake .; make
+cd ../guichan; cmake .; make
 cd ../box2d; cmake -DBOX2D_BUILD_SHARED=ON -DBOX2D_BUILD_STATIC=OFF -DBOX2D_BUILD_EXAMPLES=OFF -DBOX2D_INSTALL=OFF .; make 
 cd ../fann; cmake .; make
 make -C ../kmlocal-1.7.2 
---
+++ build_deps_js.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+
+cd guichan; make clean; rm -r CMakeFiles; rm CMakeCache.txt
+cd ../box2d/; make clean; rm -r CMakeFiles; rm CMakeCache.txt
+cd ../fann/; make clean;  rm -r CMakeFiles; rm CMakeCache.txt
+make -C ../kmlocal-1.7.2/ clean
+
+cd ../guichan; CC=emcc CXX=em++ cmake .; make
+cd ../box2d; CC=emcc CXX=em++ cmake -DBOX2D_BUILD_SHARED=ON -DBOX2D_BUILD_STATIC=OFF -DBOX2D_BUILD_EXAMPLES=OFF -DBOX2D_INSTALL=OFF .; make 
+cd ../fann; CC=emcc CXX=em++ cmake .; make
+CC=emcc CXX=em++ make -C ../kmlocal-1.7.2 
+
+
