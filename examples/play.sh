f3cfee48e2b864050bb4e3dc3586adf121b1c16a
kallaballa <amir@viel-zu.org>
2014-04-08 14:59:20 +0200

    moved examples

--- examples/play.sh
+++ examples/play.sh
@@ -4,9 +4,9 @@ function run() {
   m=1
 	[ -n "$4" ] && m=$4;
   if [ -n "$3" ]; then
-		./run.sh game/neurocid -x 1600 -y 900  -m $m -i $1 -s $2.pop -l $3 $2 > timeseries.csv 
+		neurocid -x 1600 -y 900  -m $m -i $1 -s $2.pop -l $3 $2 > timeseries.csv 
 	else
-    ./run.sh game/neurocid -x 1600 -y 900 -m $m -i $1 -s $2.pop $2 > timeseries.csv             
+    neurocid -x 1600 -y 900 -m $m -i $1 -s $2.pop $2 > timeseries.csv             
 	fi
   echo $2.pop
 }
