07bf6d7aba202f7b17ba0f7ec0b4470ae553c548
kallaballa <amir@viel-zu.org>
2014-04-09 21:40:43 +0200

    disabled video rendering in examples

--- examples/basic_training.sh
+++ examples/basic_training.sh
@@ -5,9 +5,9 @@ function run() {
   m=1
 	[ -n "$4" ] && m=$4;
   if [ -n "$3" ]; then
-		neurocid -m $m  -c $2.mp4 -i $1  -s $2.pop -l $3 $2 > timeseries.csv 
+		neurocid -m $m -i $1  -s $2.pop -l $3 $2 > timeseries.csv 
 	else
-    neurocid -m $m  -c $2.mp4 -i $1  -s $2.pop $2 > timeseries.csv             
+    neurocid -m $m -i $1  -s $2.pop $2 > timeseries.csv             
 	fi
   cp timeseries.csv $2.csv
   echo $2.pop
