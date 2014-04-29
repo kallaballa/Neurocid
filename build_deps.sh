70fc1d5eb3ba66de453018771d14737b45bc522b
kallaballa <amir@viel-zu.org>
2014-04-29 15:28:58 +0200

    included luajit in build_deps

--- build_deps.sh
+++ build_deps.sh
@@ -5,10 +5,11 @@ cd ../box2d/; make clean; rm -r CMakeFiles; rm CMakeCache.txt
 cd ../fann/; make clean;  rm -r CMakeFiles; rm CMakeCache.txt
 make -C ../kmlocal clean
 make -C ../json_spirit clean
+make -C ../LuaJIT-2.0.3 clean
 
 cd ../guichan; cmake .; make
 cd ../box2d; cmake -DBOX2D_BUILD_SHARED=ON -DBOX2D_BUILD_STATIC=OFF -DBOX2D_BUILD_EXAMPLES=OFF -DBOX2D_INSTALL=OFF .; make 
 cd ../fann; cmake .; make
 make -C ../kmlocal 
 make -C ../json_spirit
-
+make -C ../LuaJIT-2.0.3
