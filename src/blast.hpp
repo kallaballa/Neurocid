f98b23d9f9ccc4b4a6a4187e578a3eaaf0be0c55
kallaballa <amir@viel-zu.org>
2014-12-15 12:29:40 +0100

    missing file

---
+++ src/blast.hpp
@@ -0,0 +1,50 @@
+#ifndef BLAST_HPP_
+#define BLAST_HPP_
+
+#include "object.hpp"
+#include "scanner.hpp"
+#include <limits>
+
+#ifndef _NO_SERIALIZE
+#include <boost/archive/binary_iarchive.hpp>
+#include <boost/archive/binary_oarchive.hpp>
+#endif
+
+namespace neurocid {
+
+class Projectile;
+
+struct BlastLayout {
+#ifndef _NO_SERIALIZE
+	friend class boost::serialization::access;
+#endif
+	Coord radius_;
+
+#ifndef _NO_SERIALIZE
+	template<class Archive>
+	void serialize(Archive & ar, const unsigned int version) {
+	  ar & radius_;
+	}
+#endif
+};
+
+class Blast : public Object {
+public:
+	Projectile* owner_;
+	BlastLayout layout_;
+
+  Blast(Projectile& owner, BlastLayout& layout, Vector2D& loc) :
+      Object(BLAST, loc, 0, layout.radius_, false, false, false), owner_(&owner), layout_(layout) {
+  }
+
+	void move(BattleFieldLayout& bfl) {
+	}
+
+	void death() {
+		dead_ = true;
+		explode_ = true;
+	}
+};
+}
+
+#endif /* BLAST_HPP_ */
